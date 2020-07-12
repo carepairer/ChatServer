1de22d7c000ddf39a02b94900a123d1371208e3c
carepairer <2601264154@qq.com>
2020-07-12 23:37:43 +0800

    创建聊天室服务端

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -1,20 +1,81 @@
-﻿// ChatServer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
+using namespace std;
+#include<winsock2.h>
+#include<WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //1.初始化网络库
+	WORD wVersionRequested;
+	WSADATA wsData;
+	wVersionRequested = MAKEWORD(2, 2);
+	if (WSAStartup(wVersionRequested, &wsData)) {
+		cout << "初始化呢WSA失败" << endl;
+		return 0;
+	}
+
+	//2.创建socket套接字
+	SOCKET s = socket(
+		AF_INET, //INET协议族
+		SOCK_STREAM, //表示使用的是TCP协议
+		0);
+	if (s == INVALID_SOCKET) {
+		cout << "创建socket失败！" << endl;
+		return 0;
+	}
+
+	//3. bind/listen 绑定/监听窗口
+	sockaddr_in addr;
+	addr.sin_family = AF_INET; //协议族
+	inet_pton(PF_INET, "0.0.0.0", &addr.sin_addr); //ip地址，0.0.0.0表示接受所有网卡
+	addr.sin_port = htons(10086);  //网络字节序（大尾方式）， 本地字节序（小尾方式），需要转换
+	int nRet = bind(s, (sockaddr*)&addr, sizeof(addr)); //绑定
+	if (SOCKET_ERROR == nRet) {
+		cout << "绑定端口失败！" << endl;
+		return 0;
+	}
+	nRet = listen(s, 5); //监听
+	if (SOCKET_ERROR == nRet) {
+		cout << "监听失败" << endl;
+		return 0;
+	}
+
+	//4. accept接受请求、
+	else cout << "等待客户端连接。。。。" << endl;
+	sockaddr_in clientAddr = { 0 };
+	clientAddr.sin_family = AF_INET;
+	int addrlen = sizeof(clientAddr);
+	SOCKET sClient = accept(s, (sockaddr*)&clientAddr, &addrlen);
+	if (sClient == INVALID_SOCKET) {
+		cout << "连接客户端失败！！" << endl;
+		return 0;
+	}else
+
+		cout << "有客户端连接！" <<endl;
+
+	//5.send/recv 发送/接受消息
+	while (true)
+	{
+		cout << "请您输入需要发送的数据：";
+		char sendMsg[256] = { 0 };
+		cin.getline(sendMsg, 256);
+		send(sClient, sendMsg, strlen(sendMsg) + 1, 0);
+		cout << "数据发送成功，等待对面回复。。。。" << endl;
+
+		char szBuf[256] = { 0 };
+		recv(sClient, szBuf, 256, 0);
+		cout << "客户端：" << szBuf << endl;
+	}
+
+	//6.关闭socket
+	closesocket(s);
+
+	//7.反初始化操作
+	WSACleanup();
+
+	return 1;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
